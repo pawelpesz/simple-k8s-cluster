c4c826fbb2cfb6ad965b023879755dbe89702456
Peshoo <pawel.pesz@gmail.com>
2021-07-07 15:46:24 +0200

    User map is now a simple list

--- eks.tf
+++ eks.tf
@@ -1,6 +1,9 @@
-module "eks" {
-    source       = "terraform-aws-modules/eks/aws"
+data "aws_caller_identity" "current" { }
 
+module "eks" {
+    source = "terraform-aws-modules/eks/aws"
+
+    vpc_id          = module.vpc.vpc_id
     subnets         = module.vpc.private_subnets
     cluster_name    = var.cluster_name
     cluster_version = var.k8s_version
@@ -8,10 +11,15 @@ module "eks" {
     cluster_create_timeout = "1h"
     cluster_endpoint_private_access = true 
 
-    vpc_id = module.vpc.vpc_id
-
     manage_aws_auth = true
-    map_users       = var.map_users
+    map_users       = [
+        for user in var.map_users:
+            {
+                userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${user}"
+                username = "${user}"
+                groups   = [ "system:masters" ]
+            }
+    ]
 
     write_kubeconfig = true
 
--- variables.tf
+++ variables.tf
@@ -7,12 +7,8 @@ variable "aws_secret_access_key" {
 }
 
 variable "map_users" {
-    description = "Additional IAM users to add to the aws-auth configmap"
-    type = list(object({
-        userarn  = string
-        username = string
-        groups   = list(string)
-    }))
+    description = "Additional IAM users to add to the aws-auth configmap (to system:masters)"
+    type = list(string)
 }
 
 # Variables with defaults below
