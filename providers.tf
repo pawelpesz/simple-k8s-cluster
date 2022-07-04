aef9895153d9a68a63d9559480bb6963a4106f23
Pawel Pesz <pawel.pesz@tooploox.com>
2022-07-04 14:34:03 +0200

    Update components * Terraform AWS provider 4.20 -> 4.21 * Terraform EKS module 18.26.1 -> 18.26.2 * Removed Kubernetes provider configuration, it is called by the EKS module, but not used elsewhere.

--- providers.tf
+++ providers.tf
@@ -8,12 +8,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
